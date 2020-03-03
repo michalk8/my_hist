72dbca50a8c4cbd190d34307386c9b936d2928cc
Michal Klein <michal.klein@protonmail.com>
2020-03-03 22:17:13 +0100

    Fix various bug fixes - wrong boolean operator and function in signature, improve error messages

--- src/hist/named.py
+++ src/hist/named.py
@@ -11,9 +11,9 @@ class NamedHist(BaseHist):
     def __init__(self, *axes: axis, **kwargs):
         super().__init__(*axes, **kwargs)
         self._axes_names_to_ixs = defaultdict(lambda: self._sentinel, {ax.name: ix for ix, ax in enumerate(self.axes)})
-        self._fill_params_to_ignore = {k for k, param in signature(self.fill).parameters.items()
-                                       if param.kind != Parameter.KEYWORD_ONLY}
-        print(self._fill_params_to_ignore)
+        # ignore these keys when calling .fill
+        self._fill_params_to_ignore = {k for k, param in signature(super().fill).parameters.items()
+                                       if param.kind == Parameter.KEYWORD_ONLY}
 
     def _validate_axes(self):
         for ix, ax in enumerate(self.axes):
@@ -24,12 +24,13 @@ class NamedHist(BaseHist):
         args = []
         for k in list(kwargs.keys()):
             if k not in self._fill_params_to_ignore:
-                if self._axes_names_to_ixs[k] is self._sentinel:
-                    raise ValueError(f'Invalid axis name `{k}`. Valid options are: `{self._axes_names_to_ixs.keys()}`.')
+                if k not in self._axes_names_to_ixs:
+                    raise ValueError(f'Invalid axis name `{k}`. '
+                                     f'Valid options are: `{list(self._axes_names_to_ixs.keys())}`.')
                 args.append((self._axes_names_to_ixs[k], kwargs.pop(k)))
 
         args = tuple(v for k, v in sorted(args, key=lambda kv: kv[0]))
-        super().fill(*args, **kwargs)
+        return super().fill(*args, **kwargs)
 
     def __getitem__(self, item):
         if isinstance(item, dict):
@@ -39,19 +40,20 @@ class NamedHist(BaseHist):
                                      f'found `{type(k).__name__}`.')
             indices = [(ix if isinstance(ix, int) else self._axes_names_to_ixs[ix]) for ix in item.keys()]
 
-            if any((lambda i: i is self._sentinel, indices)):
+            if any(map(lambda i: i is self._sentinel, indices)):
                 # if any of the names failed to match
                 wrong_names = [k for k in item.keys() if self._axes_names_to_ixs[k] is self._sentinel]
-                raise ValueError(f'Invalid ax name(s): `{", ".join(wrong_names)}`.')
+                raise ValueError(f'Invalid ax name(s): `{", ".join(wrong_names)}`. ',
+                                 f'Valid options are: `{list(self._axes_names_to_ixs.keys())}`.')
 
             counter = Counter(indices)
-            if counter.most_common(1)[0][0] != 1:
+            if counter.most_common(1)[0][1] > 1:
                 # if we have duplicates
-                ixs_to_names = {ix: name for name, ix in self._axes_names_to_ixs}
+                ixs_to_names = {ix: name for name, ix in self._axes_names_to_ixs.items()}
                 # since dict has unique keys, invalid combination must've come from int/str mixing
                 duplicate_ixs = [(ix, ixs_to_names[ix]) for ix, count in counter.items() if count > 1]
                 raise ValueError(f'Found duplicate indices for following entries: `{duplicate_ixs}`.')
 
             item = dict(zip(indices, item.values()))
 
-        super().__getitem__(item)
+        return super().__getitem__(item)
